f7b2d28221655efe358da2578f53084736c30b46
Emarie Swenson <emarieswenson@Emaries-MacBook-Air.local>
2022-07-10 18:34:27 +0200

    testing html code

--- static/js/app.js
+++ static/js/app.js
@@ -15,7 +15,6 @@ var tbody = d3.select("tbody");
 // 4. for each individual ufo sighting object (object.values), put them in the datarow (datarow), forEach((val))  says we want one object per row
 // 5: make a variable (cell) to append data to a table
 // 6: apppend each value of the object to a cell in the table ("td")
-
 function buildTable(data) {
     //clear out existing data
     tbody.html("");
@@ -32,3 +31,30 @@ function buildTable(data) {
         );
     });
 }
+
+
+//add functionality to let users click to filter by date using d3 library
+function handleClick() {
+    //d3 will find the #datetime id from html tags, then property("value") will grab info from tag and put it in "date" variable
+    let date = d3.select("#datetime").property("value");
+    
+    //set default filter and save to new variable (default is original table data)
+    let filteredData = tableData;
+    
+    //check for date filter using if statement. Show only rows where the rows'datetime = filtered date
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    //call function to rebuild table with filtered data
+    ///If no date is entered, table is original tableData
+    buildTable(filteredData);
+}
+
+
+//telling d3 to listen for event (button click) and .on says to run handleClick function when it is clicked
+//#filter-btn is telling d3 to look for the button element tag in the HTML
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+//makes sure the table loads as soon as the page loads:
+buildTable(tableData);
